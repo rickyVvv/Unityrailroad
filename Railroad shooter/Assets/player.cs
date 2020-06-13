8cdc1fef1d224bab9b5aab1d9f6b9880e879b77c
rickyVvv <48536558+rickyVvv@users.noreply.github.com>
2020-06-13 15:21:05 -0700

    Big changes. Adjusted speed of circuit and now applying x axis movement

---
+++ Railroad shooter/Assets/player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Tooltip("Inms^-1")] [SerializeField] float xSpeed=4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xPos = transform.localPosition.x + xOffset;
+        transform.localPosition= new Vector3(xPos, transform.localPosition.y, transform.localPosition.z); // this is so we dont change our x or y position
+        
+    }
+}
