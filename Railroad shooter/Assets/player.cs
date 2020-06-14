38296f431db470ffa91ecd0f9702bfe57c7e780b
rickyVvv <48536558+rickyVvv@users.noreply.github.com>
2020-06-14 16:02:37 -0700

    axis rotation different and alot of changed in player script

--- Railroad shooter/Assets/player.cs
+++ Railroad shooter/Assets/player.cs
@@ -7,15 +7,35 @@ public class player : MonoBehaviour
     [Tooltip("Inms^-1")] [SerializeField] float Speed=20f;
     [Tooltip("In m")] [SerializeField] float xRange=8f;
     [Tooltip("In m")] [SerializeField] float yRange=5f;
+    [SerializeField] float PositionPitchFactor=-5f;
+    [SerializeField] float ControlPitchFactor=-20f;
+    [SerializeField] float positionYawFactor=5f;
+    [SerializeField] float controlRollFactor=-5f;
+    float xThrow,yThrow;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+        private void ProcessRotation(){
+            float PitchDueToPosition= transform.localPosition.y* PositionPitchFactor;
+            float PitchDueToControlThrow= yThrow  * ControlPitchFactor;
+            float pitch = PitchDueToPosition + PitchDueToControlThrow;
+
+            float yaw=transform.localPosition.x * positionYawFactor;
+            float roll=xThrow * controlRollFactor;
+        transform.localRotation= Quaternion.Euler(pitch,yaw,roll);
+    }
+    private void ProcessTranslation(){
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
@@ -29,6 +49,5 @@ public class player : MonoBehaviour
         float POSITIONy= Mathf.Clamp(yPos, -yRange, yRange); 
 
         transform.localPosition= new Vector3(POSITION, POSITIONy, transform.localPosition.z); // this is so we dont change our x or y position
-
     }
 }
